a8e21671a758dacb50a8f7f265809a875a2dda3f
Raanan <raanan34@gmail.com>
2020-09-25 00:10:50 +0300

    Improve Postgress configuration.

--- src/storage/db.ts
+++ src/storage/db.ts
@@ -1,20 +1,33 @@
 import "reflect-metadata";
-import { createConnection, Connection } from "typeorm";
+import { createConnection, Connection, ConnectionOptions } from "typeorm";
 
 export class Db {
   private static _cnnection: Connection;
 
   public static async run(): Promise<void> {
-    return createConnection()
+    const options: ConnectionOptions = {
+      type: "postgres",
+      host: process.env.PG_HOST || "localhost",
+      port: 5432,
+      username: "postgres",
+      password: "postgres",
+      database: "messages-manager",
+      synchronize: true,
+      dropSchema: true,
+      logging: false,
+      entities: [
+        "build/storage/entities/*.js"
+      ]      
+    };
+    return createConnection(options)
       .then(async (connection: Connection) => {
-        this._cnnection = connection;        
+        this._cnnection = connection;
       })
       .catch((err) => {
         console.error(err);
       });
   }
 
-  
   public static getConnection(): Connection {
     if (!this._cnnection) {
       throw new Error("DB not initialized yet");
