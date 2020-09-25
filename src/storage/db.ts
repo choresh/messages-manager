1e19cc6f142e785972276f67367066a842ec8aa7
Raanan <raanan34@gmail.com>
2020-09-25 11:18:13 +0300

    Improve code of attempts to connect to Postgres DB.

--- src/storage/db.ts
+++ src/storage/db.ts
@@ -1,13 +1,35 @@
 import "reflect-metadata";
 import { createConnection, Connection, ConnectionOptions } from "typeorm";
 
+const CONNECTION_RETRY_COUNT: number = 10;
+const CONNECTION_RETRY_TIMEOUT_MS: number = 2000;
+
 export class Db {
   private static _connection: Connection;
 
   public static async run(): Promise<void> {
+    this._connection = await this.connect();   
+  }
+
+  public static getConnection(): Connection {
+    if (!this._connection) {
+      throw new Error("DB not initialized yet");
+    }
+    return this._connection;
+  }
+
+  private static async connect(): Promise<Connection> {
+
+    // Env variable 'PG_HOST' defined in 'docker-compose.yml', the 'localhost' will
+    // be selected if run performed without the 'docker-compose.yml' (e.g. while
+    // developer runs the service locally, out of docker macine).
+    let host: string = process.env.PG_HOST || "localhost";
+    
+    // TODO: currently most of the values are hard-coded - this it temp solution, a
+    // beter one will be to pass them as env variables (especially - the password!).
     const options: ConnectionOptions = {
       type: "postgres",
-      host: process.env.PG_HOST || "localhost",
+      host: host,
       port: 5432,
       username: "postgres",
       password: "postgres",
@@ -19,32 +41,36 @@ export class Db {
         "build/storage/entities/*.js"
       ]      
     };
-    // Do some attempts to connect to the postgress DB (need to wait until the service
-    // is up, if entire creation of the services done via 'docker-compose.yml').
-    for (var i = 0; ; i++) {
-      try {
-        console.log("Connect to postgress DB started, attempt " + (i + 1) + "/10");
-        this._connection = await createConnection(options);
-        console.log("Connect to postgress DB ended");
-        break;
-      } catch (err) {
-        if ( i === 9) {
-          console.log("Connect to postgress DB failed, error:", err);
-          return Promise.reject(err);
-        }
-        await new Promise((resolve, reject) => {
-          setTimeout(() => {
-            resolve();
-          }, 5000);
-        });  
-      }             
-    } 
-  }
 
-  public static getConnection(): Connection {
-    if (!this._connection) {
-      throw new Error("DB not initialized yet");
-    }
-    return this._connection;
+    return new Promise(async (resolve, reject)  => {
+      // * Do some attempts to connect to the postgress DB (in some cases need to
+      //   wait until the postgress DB is up, e.g. if entire creation of the services
+      //   done via 'docker-compose.yml').
+      // * TODO: this is temp solution, a better one will be to solve the synchronization
+      //   issue at the deployment mechanizem (i.e. at 'dockerfile' and/or 'docker-compose.yml'
+      //   files).
+      for (var i = 1; ; i++) {
+        try {
+          console.log("Connect to postgress DB started, attempt " + i + "/" + CONNECTION_RETRY_COUNT);
+          let connection: Connection = await createConnection(options);
+          console.log("Connect to postgress DB ended");
+          resolve(connection);
+          return;
+        } catch (err) {
+          console.log("MESSAGE:", err.message)
+          var isConnectionFailure: boolean = (err.message && ((<string>err.message).startsWith("connect ECONNREFUSED") || (err.message === "the database system is starting up")));
+          if ((i === CONNECTION_RETRY_COUNT) || !isConnectionFailure) {
+            console.log("Connect to postgress DB failed, error:", err);
+            reject("Connect to postgress DB failed, reason: " + err.message);
+            return;
+          }
+          await new Promise((resolve, reject) => {
+            setTimeout(() => {
+              resolve();
+            }, CONNECTION_RETRY_TIMEOUT_MS);
+          });  
+        }             
+      } 
+    });
   }
 }
